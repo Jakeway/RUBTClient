cbee6fda4402e4021626a5544a70735b8d801acb
Rob Casale <rcasale48@verizon.net>
2014-09-26 22:09:45 -0400

    Renamed file

--- src/RUBT/RUBTClient.java
+++ src/RUBT/RUBTClient.java
@@ -1,5 +1,8 @@
 package RUBT;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class RUBTClient 
 {
 	public static void main(String[] args)
@@ -9,5 +12,10 @@ public class RUBTClient
 			System.err.println("Please enter two arguments.");
 			System.exit(1);
 		}
+
+		File torretFile = new File(args[0]);
+		File destFile = new File(args[1]);
+		
+		
 	}
 }
