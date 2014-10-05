83f4b02ac0ca8c68d985097cd7d4ffa891a24f8a
Thomas Jakeway <tjakeway92@gmail.com>
2014-10-05 15:58:14 -0400

    cleaned up a bit more

--- src/RUBT/RUBTClient.java
+++ src/RUBT/RUBTClient.java
@@ -1,33 +1,13 @@
 package RUBT;
 
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.Socket;
-import java.net.SocketException;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.io.*;
+import java.net.*;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import GivenTools.Bencoder2;
-import GivenTools.BencodingException;
-import GivenTools.ToolKit;
-import GivenTools.TorrentInfo;
+import GivenTools.*;
 
 public class RUBTClient 
 {
