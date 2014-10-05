58ad8d5d6f3a97359a386150561e91789666abae
Rob Casale <rcasale48@verizon.net>
2014-10-05 15:28:22 -0400

    removed some more junk

--- src/RUBT/RUBTClient.java
+++ src/RUBT/RUBTClient.java
@@ -34,17 +34,6 @@ public class RUBTClient
 	public static void main(String[] args)
 	{
 		
-		final ByteBuffer KEY_PEERS = ByteBuffer.wrap(new byte[] {
-				'p', 'e', 'e', 'r', 's' });
-		
-		final ByteBuffer KEY_PEER_ID = ByteBuffer.wrap(new byte[] {
-				'p', 'e', 'e', 'r', ' ', 'i', 'd' });
-		
-		final ByteBuffer KEY_IP = ByteBuffer.wrap(new byte[] {
-				'i', 'p' });
-		
-		final ByteBuffer KEY_PORT = ByteBuffer.wrap(new byte[] {
-				'p', 'o', 'r', 't' });
 		
 		final ByteBuffer HANDSHAKE_HEADER = ByteBuffer.wrap(new byte[] { 
 				19,'B','i','t','T','o','r','r','e','n','t',' ',
@@ -72,70 +61,16 @@ public class RUBTClient
 			System.exit(1);
 		}
 		
-		ti = getTorrentInfo(torrentFile);
-		
-		int length = ti.file_length;
-		
-		String encodedHash="";
-		try 
-		{
-			encodedHash = Util.byteArrayToURLString(ti.info_hash.array());
-		} catch (Exception e) {
-		
-			e.printStackTrace();
-		}
-		 
+		ti = Util.getTorrentInfo(torrentFile);
 		
 		String peer_id = "tomjakewaynrobcasale";
 
-		announceURL = ti.announce_url.toExternalForm();
-		String trackerURL = announceURL + "?info_hash=" + encodedHash
-				+ "&peer_id=" + peer_id
-				+ "&left=" + length
-				+ "&port=" + "6881"
-				+ "&downloaded=" + "0";
-		
-		System.out.println(trackerURL);
-		
-		byte[] trackerResponse = sendGetRequest(trackerURL).getBytes();
-		HashMap<ByteBuffer, Object> trackerResponseMap = null;
-		ArrayList<HashMap<ByteBuffer, Object>> peerDicts = null;
-		
-		ByteBuffer remotePeerIdBytes = null;
 		
 		byte[] handShake = new byte[68];
 		HANDSHAKE_HEADER.get(handShake, 0, HANDSHAKE_HEADER.remaining());
 		ti.info_hash.get(handShake, 28, ti.info_hash.remaining());
 		
-		try {
-			trackerResponseMap = 
-					(HashMap<ByteBuffer, Object>) Bencoder2.decode(trackerResponse);
-			
-			peerDicts = 
-					(ArrayList<HashMap<ByteBuffer, Object>>)
-					trackerResponseMap.get(KEY_PEERS);
-
-			String ip = "128.6.171.131";
-			int port = 61350;
-			
-			for (HashMap<ByteBuffer, Object> dicts : peerDicts)
-			{
-				remotePeerIdBytes = (ByteBuffer) dicts.get(KEY_PEER_ID);
-				String remotePeerId = new String(remotePeerIdBytes.array(), "UTF-8");
-				
-				ByteBuffer remoteIPBytes = (ByteBuffer) dicts.get(KEY_IP);
-				String remoteIP = new String(remoteIPBytes.array(), "UTF-8");
-				
-				int remotePort =  (Integer) dicts.get(KEY_PORT);
-				
-				if(remoteIP.equals("128.6.171.131"))
-					break;
-								
-				System.out.println(remotePeerId);
-				System.out.println(remoteIP);
-				System.out.println(remotePort);
-			}
-			
+		
 			Util.addStringToByteArray(handShake, peer_id);
 			
 			System.out.println(new String(handShake));
@@ -169,125 +104,10 @@ public class RUBTClient
 				System.err.println("IOException: " + e.getMessage());
 				System.exit(1);
 			}
-			
-		} catch (BencodingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		
-		ToolKit.printMap(trackerResponseMap, 0);
 		
 		
 	}
 	
-	public static TorrentInfo getTorrentInfo(File torrentFile)
-	{
-		FileInputStream fis = null;
-		TorrentInfo ti = null;
-		
-		try 
-		{
-			fis = new FileInputStream(torrentFile);
-		} catch (FileNotFoundException e) 
-		{
-			System.err.println("File not found");
-			System.exit(1);
-		}
-		
-		byte [] bytes = new byte[(int) torrentFile.length()];
-		try 
-		{
-			fis.read(bytes, 0, bytes.length);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		try 
-		{
-			ti = new TorrentInfo(bytes);
-		} catch (BencodingException e) {
-			e.printStackTrace();
-		}
-		
-		try
-		{
-			fis.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return ti;
-	}
-	
-	public static String sendGetRequest(String trackerURL)
-	{
-		URL u = null;
-		try {
-			u = new URL(trackerURL);
-		} catch (MalformedURLException e1) {
-			e1.printStackTrace();
-		}
-		String contents = "";
-		try 
-		{
-		
-			BufferedReader br = new BufferedReader(new InputStreamReader(u.openStream()));
-			String line = "";
-			while ((line = br.readLine()) != null)
-			{
-				contents += line;
-			}
-			br.close();
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		return contents;
-	}
-	
-	
-	 /* public static String byteArrayToURLString(byte in[]) 
-	  {
-		    byte ch = 0x00;
-		    int i = 0;
-		    if (in == null || in.length <= 0)
-		      return null;
 
-		    String pseudo[] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-		        "A", "B", "C", "D", "E", "F" };
-		    StringBuffer out = new StringBuffer(in.length * 2);
-
-		    while (i < in.length) {
-		      // First check to see if we need ASCII or HEX
-		      if ((in[i] >= '0' && in[i] <= '9')
-		          || (in[i] >= 'a' && in[i] <= 'z')
-		          || (in[i] >= 'A' && in[i] <= 'Z') || in[i] == '$'
-		          || in[i] == '-' || in[i] == '_' || in[i] == '.'
-		          || in[i] == '!') {
-		        out.append((char) in[i]);
-		        i++;
-		      } else {
-		        out.append('%');
-		        ch = (byte) (in[i] & 0xF0); // Strip off high nibble
-		        ch = (byte) (ch >>> 4); // shift the bits down
-		        ch = (byte) (ch & 0x0F); // must do this is high order bit is
-		        // on!
-		        out.append(pseudo[(int) ch]); // convert the nibble to a
-		        // String Character
-		        ch = (byte) (in[i] & 0x0F); // Strip off low nibble
-		        out.append(pseudo[(int) ch]); // convert the nibble to a
-		        // String Character
-		        i++;
-		      }
-		    }
-
-		    String rslt = new String(out);
-
-		    return rslt;
-	  }*/
 		
 }
