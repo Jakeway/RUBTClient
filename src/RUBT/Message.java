1c9b6c253312cbc8d71194093656715586c3204b
Rob Casale <rcasale48@verizon.net>
2014-10-05 23:31:55 -0400

    Added Message file

---
+++ src/RUBT/Message.java
@@ -0,0 +1,20 @@
+package RUBT;
+
+public class Message {
+
+	byte[] keepAlive;
+	byte[] choke;
+	byte[] unchoke;
+	byte[] interested;
+	byte[] uninterested;
+	byte[] have;
+	byte[] bitfield;
+	byte[] request;
+	byte[] piece;
+	byte[] cancel;
+
+	public void createMessage()
+	{
+				
+	}
+}
