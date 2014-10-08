8b020d58265cd6759126c4ce35f8841ba180457c
Thomas Jakeway <tjakeway92@gmail.com>
2014-10-08 00:19:34 -0400

    can iterate through BitfieldMessage bitfields

--- src/RUBT/BitfieldMessage.java
+++ src/RUBT/BitfieldMessage.java
@@ -1,12 +1,37 @@
 package RUBT;
 
-public class BitfieldMessage extends Message {
+public class BitfieldMessage extends Message 
+{
 
 	private byte[] bitfield;
+	
+	
 
 	public BitfieldMessage(byte[] bitfield)
 	{
 		super(bitfield.length + 1, Message.BITFIELD_ID);
 		this.bitfield = bitfield;
+	}
+	
+	private boolean isBitSet(int byteIndex, int bitIndex)
+	{
+		int bitMask = 1 << bitIndex;
+		return ( bitfield[byteIndex] & bitMask) > 0;
+	}
+	
+	public void iterateBitfield()
+	{
+		
+		for (int j = 0; j < bitfield.length; j++)
+		{
+			
+		for (int i = 0; i < 8; i++)
+		{
+			if (isBitSet(j, i))
+			{
+				System.out.println("Bit index " + i + " is set in byte " + j);
+			}
+		}
+		}
 	}
 }
